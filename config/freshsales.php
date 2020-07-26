b7c1f442f7a254f844a3dc91fd407a7736e0f013
Evgeni Nurkov <evgeni.nurkov@gmail.com>
2020-07-26 18:06:22 +0300

    config/freshsales.php

--- config/freshsales.php
+++ config/freshsales.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'domain' => env('FRESHSALES_DOMAIN'),
+    'domain' => env('FRESHSALES_DOMAIN', 'freshsales.io'),
     'api_key' => env('FRESHSALES_APIKEY'),
     'enable_rate_limit' => env('FRESHSALES_ENABLE_RATE_LIMIT'),
 ];
